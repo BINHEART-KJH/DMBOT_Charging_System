aa7fa47baa396611bc60dd366bb50e55b8bb9055
KJH <jhkim@daminrobot.com>
2026-01-27 18:10:16 +0900

    [ADD,COPY] 24V,48V 용 코드 분리

--- DMBOT_STATION/include/station_gpio.h
+++ DMBOT_STATION/include/station_gpio.h
@@ -1,4 +1,4 @@
-/*#ifndef STATION_GPIO_H
+#ifndef STATION_GPIO_H
 #define STATION_GPIO_H
 
 #include <Arduino.h>
@@ -16,30 +16,5 @@
 // GPIO 관련 함수
 void gpio_init();
 void gpio_run();
-
-#endif
-*/
-
-#ifndef STATION_GPIO_H
-#define STATION_GPIO_H
-
-#include <Arduino.h>
-
-// GPIO 핀 정의
-#define DOCKING_PIN      8
-#define BATTERY_FULL_PIN 5
-#define CHARGER_OK_PIN   6
-
-#define RELAY_PIN        7
-#define RELAY_PIN2       4
-#define BUILTIN_LED      LED_BUILTIN
-
-#define ADC_PIN          A0 
-
-// GPIO 관련 함수
-void gpio_init();
-void gpio_run();
-
-void station_setRelay(bool on, const char* reason);
 
 #endif
